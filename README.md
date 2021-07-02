b8ddde5482fe23760c860ac8b0116098e52577e8
Lachlan Grose <lachlan.grose@monash.edu>
2021-07-02 10:20:55 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# python_template
+ python library for testing actions
---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+import codecs
+from setuptools import setup, find_packages
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+setup(
+	name="MyLibrary",
+	version=get_version("MyLibrary/__init__.py"),
+
+	)
